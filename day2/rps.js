8cf2e9387ac25f7c6030bc2095832f2bbca36186
yabepj <yabepj@yahoo.com>
2016-08-02 10:14:50 -0400

    day2day2

---
+++ day2/rps.js
@@ -0,0 +1,45 @@
+
+
+console.log("IM here");
+/* prompt the user to enter rock papaer, or scissors
+   randomly get computers selection
+   display winner or tie
+*/
+
+
+var rpsPrompt =  prompt('Enter rock, paper or scissors', 'rock');
+ var rpsValues = ["rock", "paper", "scissors"];
+ console.log (rpsValues);
+console.log("rpsPrompt:  : " + rpsPrompt);
+
+ var randomSelection = Math.round(Math.random()*3);
+  console.log(randomSelection);
+  
+  var rpsCompare = "";
+  var found=false;
+ for (i =0; i<=2; i++){
+	 console.log('In for loop:  rps:  ' + i+1 );
+	 rpsCompare=rpsValues[i];
+	 console.log("randomValue: "  + rpsCompare);
+	 
+	 if (rpsCompare===(rpsValues[i]) ) {
+		 found=true;
+		 
+		 if ((i+1)===randomSelection){
+			 console.log("TIE");
+		 } // end if 
+		 else if ((i+1) === 1 && randomSelection ===2 ){
+			      console.log ('You Lose! Computer had paper'); 
+		      }
+	          
+			 
+		
+	 }
+	 
+	 
+ } // end forloop on rpsPrompp array
+  
+
+
+
+
