1b7587a0feb4360e7a3df083e29b709e12a2022a
yabepj <yabepj@yahoo.com>
2016-08-02 09:34:39 -0400

    any new changes

--- rps.js
+++ rps.js
@@ -10,20 +10,23 @@ console.log("IM here");
 var rpsPrompt =  prompt('Enter rock, paper or scissors', 'rock');
  var rpsValues = ["rock", "paper", "scissors"];
  console.log (rpsValues);
-console.log(rpsPrompt);
+console.log("rpsPrompt:  : " + rpsPrompt);
 
  var randomSelection = Math.round(Math.random()*3);
   console.log(randomSelection);
   
-  var rpsCompare = '';
+  var rpsCompare = "";
   var found=false;
  for (i =0; i<=2; i++){
-	 console.log('In for loop:  i:  ' + i );
-	 if (rpsCompare.equals(rpsValues[i]) ) {
+	 console.log('In for loop:  rps:  ' + i+1 );
+	 rpsCompare=rpsValues[i];
+	 console.log("randomValue: "  + rpsCompare);
+	 
+	 if (rpsCompare===(rpsValues[i]) ) {
 		 found=true;
 		 
 		 if ((i+1)===randomSelection){
-			 console.log(TIE);
+			 console.log("TIE");
 		 } // end if 
 		 else if ((i+1) === 1 && randomSelection ===2 ){
 			      console.log ('You Lose! Computer had paper'); 
